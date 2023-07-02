fdde88e99c5ebfc7b393962b4a18caf949b8d7f4
Yogalakshmi1990 <138385956+Yogalakshmi1990@users.noreply.github.com>
2023-07-02 21:06:20 +0530

    Add files via upload

---
+++ Smarphone.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class Smarphone extends Androidphone {
+
+	public void takevideo() {
+		
+		System.out.println("takevideo");
+
+}
+
+	public static void main(String[] args) {
+		Smarphone obj= new Smarphone();
+		obj.takevideo();
+		
+	}
+}
